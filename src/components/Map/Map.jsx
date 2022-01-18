f36bfb386cb3d561828aa5f0928906d2f0dfaa7a
zdienos <zdienos@yahoo.com>
2022-01-18 19:46:50 +0800

    integrasi google map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles.js';
 
 const Map = () => {
+    const classes = useStyles();
+    //const isMobile = useMediaQuery('(min-width:600px)');
+    
+    const coordinates = { lat: -5.1768196, lng: 119.4043802 };
+
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{key: 'AIzaSyBJAiF_XQVSSMgFC_SPFTSricMbHoGZHn8'}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+            </GoogleMapReact>
+        </div>
     );
 }
 
