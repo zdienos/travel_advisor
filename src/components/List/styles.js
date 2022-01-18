e16815fd3444bfec8b822bf6922dcc49f8de8c24
zdienos <zdienos@yahoo.com>
2022-01-18 16:49:23 +0800

    initial commit

---
+++ src/components/List/styles.js
@@ -0,0 +1,39 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1), minWidth: 120, marginBottom: '30px',
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+  loading: {
+    height: '600px', display: 'flex', justifyContent: 'center', alignItems: 'center',
+  },
+  container: {
+    padding: '25px',
+  },
+  marginBottom: {
+    marginBottom: '30px',
+  },
+  list: {
+    height: '75vh', overflow: 'auto',
+  },
+}));
+MapStyles.js
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  paper: {
+    padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100px',
+  },
+  mapContainer: {
+    height: '85vh', width: '100%',
+  },
+  markerContainer: {
+    position: 'absolute', transform: 'translate(-50%, -50%)', zIndex: 1, '&:hover': { zIndex: 2 },
+  },
+  pointer: {
+    cursor: 'pointer',
+  },
+}));
